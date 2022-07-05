99f69863849826105d5e2602388dae0abeb4659e
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-07-05 11:41:39 +0200

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 // solution goes here
+type CommentData = {
+    id: number
+    content: string
+    imageId: number
+  }
+  
+  type Image = {
+    id: number
+    title: string
+    likes: number
+    image: string
+    comments: CommentData[]
+  }
+  
+  type State = {
+    images: Image[]
+  }
+  
+  let state: State = {
+    images: []
+  }
+  
